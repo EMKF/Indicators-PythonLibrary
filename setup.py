3d405956ebd5c116b898bd87661c1246fb8d5277
Travis Howe <b.travis.howe@gmail.com>
2020-03-17 05:31:10 -0500

    setup file modify

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='kauffman_data',
-    url='https://github.com/jladan/package_demo',
-    author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
+    name='kcr',
+    url='https://github.com/EMKF/downwardata',
+    author='Travis Howe',
+    author_email='thowe@kauffman.org',
     # Needed to actually package something
-    packages=['measure'],
+    packages=['kauffman_data'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['numpy', 'pandas', 'requests'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
     license='MIT',
-    description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    description='Modules that pull and transform commonly used administrative data from online sources.',
+    long_description=open('README.md').read(),
 )
