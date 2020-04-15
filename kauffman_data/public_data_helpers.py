644a19246f6a475d76c4ad6748c56bb8a8e78794
Travis Howe <b.travis.howe@gmail.com>
2020-04-15 09:29:07 -0500

    kese to download final

--- kauffman_data/public_data_helpers.py
+++ kauffman_data/public_data_helpers.py
@@ -13,31 +13,13 @@ def _grouper(df, lvalues):  # todo: should I put this inside the class?
             reset_index()
     return df.reset_index(drop=True)
 
-def _year_lst(df, v):
-    year_lst = [col.split('_')[1] for col in df.columns.tolist() if v in col]
-    # year_lst.sort()
-    print(year_lst); sys.exit()
-    return
 
-def _state_transform(df, v, df_state):
-    covar_lst = [col for col in df.columns.tolist() if v in col]
-    new_covar_lst = [col.replace('_', '') for col in covar_lst]
-    df_v = df.\
-        rename(columns={**dict(zip(covar_lst, new_covar_lst)), **{'sname': 'name'}}) \
-        [['name'] + new_covar_lst].\
-        pipe(pd.wide_to_long, v, i='name', j='year').\
-        reset_index() \
-        [['name', 'year', v]]
-    if df_state.shape[1] == 0:
-        return df_v
-    return df_state.merge(df_v, how='left', on=['name', 'year'])
-
-
-def _us_transform(df, v, df_us):
-    covar_lst = [col for col in df.columns.tolist() if v in col and col.count('_') == 1]
+def _region_transform(df, v, df_out):
+    covar_lst = [col for col in df.columns.tolist() if v + '_' in col and col.count('_') == 1]
     new_covar_lst = [col.replace('_', '') for col in covar_lst]
     df_v = df.\
         rename(columns={**dict(zip(covar_lst, new_covar_lst)), **{'sname': 'name', 'demtype': 'type', 'demographic': 'category'}}). \
+        replace('Annual', 'Total').\
         assign(
             type=lambda x: 'Total' if 'type' not in x.columns else x['type'],
             category=lambda x: 'Total' if 'category' not in x.columns else x['category'],
@@ -46,53 +28,9 @@ def _us_transform(df, v, df_us):
         pipe(pd.wide_to_long, v, i=['name', 'type', 'category'], j='year').\
         reset_index()
 
-    # print(df_v)
-
-    if df_us.shape[1] == 0:
+    if df_out.shape[1] == 0:
         return df_v
-    return df_us.merge(df_v, how='left', on=['name', 'type', 'category', 'year'])
-
-
-    print(df_v)
-    sys.exit()
-    # print(v)
-
-    covar_lst = [col for col in df.columns.tolist() if v in col]
-    new_covar_lst = [col.replace('_', '') for col in covar_lst]
-
-    df_group = df. \
-        rename(columns={**dict(zip(covar_lst, new_covar_lst)), **{'sname': 'name'}}). \
-        assign(
-            demtype=lambda x: 'overall' if 'demtype' not in x.columns else x['demtype'],
-            demographic=lambda x: 'overall' if 'demographic' not in x.columns else x['demographic']
-        ) \
-        [['name', 'demtype', 'demographic'] + new_covar_lst]. \
-        groupby(['name', 'demtype'])
-
-    df_out = pd.DataFrame()
-    for group in df_group:
-
-        df_temp = pd.wide_to_long(group[1], v, i=['name', 'demtype', 'demographic'], j='year').\
-            reset_index().\
-            rename(columns={'demographic': group[0][1]}).\
-            drop('demtype', 1)
-        df_out = df_out.append(df_temp)
-
-    #         fillna('economy wide'). \
-    df_v = df_out. \
-        assign(
-            Age=lambda x: 'economy wide' if 'Age' not in x.columns else x['Age'],
-            Education=lambda x: 'economy wide' if 'Education' not in x.columns else x['Education'],
-            Nativity=lambda x: 'economy wide' if 'Nativity' not in x.columns else x['Nativity'],
-            Race=lambda x: 'economy wide' if 'Race' not in x.columns else x['Race'],
-            Sex=lambda x: 'economy wide' if 'Sex' not in x.columns else x['Sex'],
-        ) \
-        [['name', 'year', 'Age', 'Education', 'Nativity', 'Race', 'Sex'] + [v]]
-    # todo: nans are being filled in as economy wides
-    print(df_v)
-    if df_us.shape[1] == 0:
-        return df_v
-    return df_us.merge(df_v, how='left', on=['name', 'year', 'Age', 'Education', 'Nativity', 'Race', 'Sex'])
+    return df_out.merge(df_v, how='left', on=['name', 'type', 'category', 'year'])
 
 
 def fairlie_to_panel(state_file_path, us_file_path):
@@ -110,95 +48,12 @@ def fairlie_to_panel(state_file_path, us_file_path):
 
     df_us, df_state = pd.DataFrame(), pd.DataFrame()
     for k, v in indicator_dict.items():
-        df_state = _state_transform(pd.read_excel(state_file_path, sheet_name=k), v, df_state)
-        df_us = _us_transform(pd.read_excel(us_file_path, sheet_name=k), v, df_us)
-    print(df_us)
-    sys.exit()
+        df_state = _region_transform(pd.read_excel(state_file_path, sheet_name=k), v, df_state)
+        df_us = _region_transform(pd.read_excel(us_file_path, sheet_name=k), v, df_us)
 
-
-    #
-    # df_state.\
-    #     append(df_us).\
-    #     reset_index(drop=True).\
-    #     pipe(_file_saver, v)
-    #
-
-
-
-
-    for i, indicators in enumerate(indicator_dict.values()):
-        df_temp = joblib.load(c.filenamer('data/kese_{}.pkl'.format(indicators))). \
-            rename(
-            columns={
-                **dict(zip(range(1996, end_year),
-                           map(lambda x: '{0}{1}'.format(indicators, x), range(1996, end_year)))),
-                **{'region': 'fips'}
-            }
-        ). \
-            assign(name=lambda x: x['fips'].map(c.state_codes).map(c.abbrev_us_state)). \
-            pipe(pd.wide_to_long, [indicators], i=['name', 'fips', 'demographic-type', 'demographic'], j='year'). \
-            reset_index(). \
-            replace('', 'overall')
-        if i == 0:
-            df = df_temp
-        else:
-            df = df.merge(df_temp, how='outer', on=['name', 'fips', 'demographic-type', 'demographic', 'year'])
-
-    df = df.assign(sex='', race='', nativity='', age='', education='', veteran_status='')
-
-    df.loc[
-        df['demographic-type'] == 'overall', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Male', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'male', 'overall', 'overall', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Female', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'female', 'overall', 'overall', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'White', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'white', 'overall', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Black', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'black', 'overall', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Latino', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'latino', 'overall', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Asian', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'asian', 'overall', 'overall', 'overall', 'overall']
-    df.loc[
-        df['demographic'] == 'Native-Born', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'native_born', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Immigrant', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'immigrant', 'overall', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Ages 20-34', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'ages 20-34', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Ages 35-44', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'ages 35-44', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Ages 45-54', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'ages 45-54', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Ages 55-64', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'ages 55-64', 'overall', 'overall']
-    df.loc[df['demographic'] == 'Less than High School', ['sex', 'race', 'nativity', 'age', 'education',
-                                                          'veteran_status']] = ['overall', 'overall', 'overall',
-                                                                                'overall', 'less than high school',
-                                                                                'overall']
-    df.loc[df['demographic'] == 'High School Graduate', ['sex', 'race', 'nativity', 'age', 'education',
-                                                         'veteran_status']] = ['overall', 'overall', 'overall',
-                                                                               'overall', 'high school graduate',
-                                                                               'overall']
-    df.loc[
-        df['demographic'] == 'Some College', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'overall', 'some college', 'overall']
-    df.loc[df['demographic'] == 'College Graduate', ['sex', 'race', 'nativity', 'age', 'education',
-                                                     'veteran_status']] = ['overall', 'overall', 'overall',
-                                                                           'overall', 'college graduate', 'overall']
-    df.loc[df['demographic'] == 'Veterans', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'overall', 'overall', 'veterans']
-    df.loc[
-        df['demographic'] == 'Non-Veterans', ['sex', 'race', 'nativity', 'age', 'education', 'veteran_status']] = [
-        'overall', 'overall', 'overall', 'overall', 'overall', 'non-veterans']
-
-    return df. \
-        drop(['demographic-type', 'demographic'], 1). \
-        sort_values(['fips', 'year']). \
-        reset_index(drop=True)  #. \
-        # to_csv(c.filenamer('data/kese_download.csv'), index=False)
+    return df_state.\
+        append(df_us).\
+        reset_index(drop=True)
 
 
 @pd.api.extensions.register_dataframe_accessor("pub")
