8dc55be52e4cad0d82e30dff92294e7832d0f6f2
Travis Howe <b.travis.howe@gmail.com>
2021-03-29 10:41:31 -0500

    increment version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'numpy', 'pandas', 'requests', 'statsmodels', 'geonamescache'
     ],
-    version='2.1.0',
+    version='2.1.1',
     # The license can be anything you like
     license='MIT',
     description='Modules that pull and transform commonly used administrative data from online sources.',
