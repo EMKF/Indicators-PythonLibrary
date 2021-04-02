696c81c6cda628b7a5ae11e8a28e8d3b22900222
Travis Howe <b.travis.howe@gmail.com>
2021-04-02 08:37:49 -0500

    xlrd to requiremnts

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ setup(
     author_email='thowe@kauffman.org',
     packages=find_packages(),
     install_requires=[
-        'numpy', 'pandas', 'requests', 'statsmodels', 'geonamescache'
+        'numpy', 'pandas', 'requests', 'statsmodels', 'geonamescache', 'xlrd==1.2.0'
     ],
     version='2.1.3',
-    # The license can be anything you like
     license='MIT',
     description='Modules that pull and transform commonly used administrative data from online sources.',
     long_description=open('README.md').read(),
