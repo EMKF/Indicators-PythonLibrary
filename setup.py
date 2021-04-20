e70160c344e15b93d26e1095ed91a93067764d52
Travis Howe <b.travis.howe@gmail.com>
2021-04-20 09:20:53 -0500

    increment

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'numpy', 'pandas', 'requests', 'statsmodels', 'geonamescache', 'xlrd==1.2.0'
     ],
-    version='2.1.3',
+    version='2.1.4',
     license='MIT',
     description='Modules that pull and transform commonly used administrative data from online sources.',
     long_description=open('README.md').read(),
