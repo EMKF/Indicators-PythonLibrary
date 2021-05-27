dc403e5f57cdb756ef5775afaaf4784cb9dbad8a
htmurray15 <hmurray@MacMurH111919.local>
2021-05-27 12:39:00 -0500

    saver 5.27

--- kauffman/data/helpers/shed_helpers.py
+++ kauffman/data/helpers/shed_helpers.py
@@ -12,61 +12,8 @@ pd.set_option('max_info_columns', 1000)
 pd.set_option('expand_frame_repr', False)
 pd.set_option('display.max_rows', 30000)
 pd.set_option('max_colwidth', 4000)
-# pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
 pd.options.mode.chained_assignment = None
-
-
-# 11. Maine
-# 12. New Hampshire
-# 13. Vermont
-# 14. Massachusetts
-# 15. Rhode Island
-# 16. Connecticut
-# 21. New York
-# 22. New Jersey
-# 23. Pennsylvania
-# 31. Ohio
-# 32. Indiana
-# 33. Illinois
-# 34. Michigan
-# 35. Wisconsin
-# 41. Minnesota
-# 42. Iowa
-# 43. Missouri
-# 44. North Dakota
-# 45. South Dakota
-# 46. Nebraska
-# 47. Kansas
-# 51. Delaware
-# 52. Maryland
-# 53. District Of Columbia
-# 54. Virginia
-# 55. West Virginia
-# 56. North Carolina
-# 57. South Carolina
-# 58. Georgia
-# 59. Florida
-# 61. Kentucky
-# 62. Tennessee
-# 63. Alabama
-# 64. Mississippi
-# 71. Arkansas
-# 72. Louisiana
-# 73. Oklahoma
-# 74. Texas
-# 81. Montana
-# 82. Idaho
-# 83. Wyoming
-# 84. Colorado
-# 85. New Mexico
-# 86. Arizona
-# 87. Utah
-# 88. Nevada
-# 91. Washington
-# 92. Oregon
-# 93. California
-# 94. Alaska
-# 95. Hawaii
 
 
 # create a function to subset and rename columns
@@ -76,9 +23,22 @@ def column_formater(df):
     # rename columns to lower case
     df.columns = df.columns.str.lower()
     # subset each df by columns of interest
-    df = df[['caseid', 'time', 'ppstaten']]
+    df = df[['caseid', 'time', 'ppstaten', 'e2']]
     # replace values with state abbreviation
-    df["ppstaten"].replace({31: "OH", 44: "ND", 59: "FL", 85: "NM"}, inplace=True)
+    df["ppstaten"].replace({11: "ME", 12: "NH", 13: "VT", 14: "MA", 15: "RI", 16: "CT",
+                            21: "NY", 22: "NJ", 23: "PA", 31: "OH", 32: "IN", 33: "IL", 34: "MI", 35: "WI",
+                            41: "MN", 42: "IA", 43: "MO", 44: "ND", 45: "SD", 46: "NE", 47: "KS",
+                            51: "DE", 52: "MD", 53: "DC", 54: "VA", 55: "WV", 56: "NC", 57: "SC", 58: "GA",
+                            59: "FL", 61: "KY", 62: "TN", 63: "AL", 64: "MS", 71: "AR", 72: "LA", 73: "OK",
+                            74: "TX", 81: "MT", 82: "ID", 83: "WY", 84: "CO", 85: "NM", 86: "AZ", 87: "UT",
+                            88: "NV", 91: "WA", 92: "OR", 93: "CA", 94: "AK", 95: "HI"}, inplace=True)
+    # make ppstaten upppercase
+    # df['ppstaten'] = df['ppstaten'].str.lower()
+
+    # replace values with answers 
+    df["e2"].replace({-1: "refused", 0: "no", 1: "yes"}, inplace=True)
+    # make everything lowercase
+    df = df.apply(lambda x: x.astype(str).str.lower())
     print(df.head())
     return df
 
@@ -95,10 +55,9 @@ def _shed_data_create():
         'public_use_data_2019_(CSV)': 'public2019.csv',
         'public_use_data_2020_(CSV)': 'public2020.csv',
     }
-    # shed_links = {
-    #     'data_2013_(CSV)': 'SHED_public_use_data_2013.csv',
-    # }
+    # create df to append to
     df = pd.DataFrame()
+    # loop over dict of urls and file names and append to df
     for key, value in shed_links.items():
         url = 'https://www.federalreserve.gov/consumerscommunities/files/SHED_' + str(key) + '.zip'
         r = requests.get(url)
@@ -117,30 +76,6 @@ def _shed_data_create():
 
 if __name__ == '__main__':
     df = _shed_data_create()
-    # df = plotter_1(df)
 
 sys.exit()
 
-# pull data for Phase One, Two, and Three by looping over each url parameter
-shed_links = {
-    'data_2013_(CSV)': 'SHED_public_use_data_2013.csv',
-    'public_use_data_2014_(CSV)': 'SHED_public_use_data_2014_update (occupation industry).csv',
-    'public_use_data_2015_(CSV)': 'SHED 2015 public use.csv',
-    'public_use_data_2016_(CSV)': 'SHED_2016_Public_Data.csv',
-    'public_use_data_2017_(CSV)': 'SHED_2017_Public_Use.csv',
-    'public_use_data_2018_(CSV)': 'public2018.csv',
-    'public_use_data_2019_(CSV)': 'public2019.csv',
-    'public_use_data_2020_(CSV)': 'public2020.csv',
-}
-# shed_links = {
-#     'data_2013_(CSV)': 'SHED_public_use_data_2013.csv',
-# }
-df = pd.DataFrame()
-for key, value in shed_links.items():
-# # pull in each df by looping over url for each shed year
-# z = urlopen('https://www.federalreserve.gov/consumerscommunities/files/SHED_' + str(key) + '.zip')
-# # How can I get this line to stop saving the .csv in this python folder???
-# myzip = ZipFile(BytesIO(z.read())).extract(value)
-# # Why doesn't this remove the file?
-# # os.remove(value)
-# temp_df = pd.read_csv(myzip, low_memory=False, encoding='cp1252')
