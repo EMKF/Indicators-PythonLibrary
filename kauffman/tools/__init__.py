9ad9bc57605d734c40b462f075ee757240e76dd0
Travis Howe <b.travis.howe@gmail.com>
2021-06-24 14:53:50 -0500

    init deletion

--- kauffman/tools/__init__.py
+++ kauffman/tools/__init__.py
@@ -1,10 +1,8 @@
-from scratch.plotting_aids import time_series
 from .etl import file_to_s3, file_from_s3, county_msa_cross_walk
 from .distribution_tests import alpha, log_log_plot, maximum_to_sum_plot, excess_conditional_expectation, \
     maximum_quartic_variation
 
 __all__ = [
-    'time_series',
     'file_to_s3', 'file_from_s3', 'county_msa_cross_walk',
     'alpha', 'log_log_plot', 'maximum_to_sum_plot', 'excess_conditional_expectation', 'maximum_quartic_variation',
 ]
