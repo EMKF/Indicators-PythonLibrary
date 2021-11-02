a9a4cba3e43f77a670b2359d3cba75482ecfddc3
Katie Stevens Andersen <KAstev@gmail.com>
2021-11-02 14:58:38 -0600

    Replace industry argument with general strata arg

--- kauffman/data/_bds.py
+++ kauffman/data/_bds.py
@@ -30,8 +30,8 @@ def _fetch_data(url):
         raise Exception(f'ERROR. Response code: {r.status_code} for url: {url}')
     return df
 
-def _build_url(variables, region):
-    var_string = ",".join(variables + ["NAICS"])
+def _build_url(variables, region, strata):
+    var_string = ",".join(variables + strata)
     
     if region in ['state', 'us']:
         region_string = f'{region}:*'
@@ -39,29 +39,30 @@ def _build_url(variables, region):
         region_string = 'metropolitan%20statistical%20area/micropolitan%20statistical%20area:*'
     elif region == 'county':
         region_string = 'county:*&in=state:*'
+
+    naics_string = '&NAICS=00' if 'NAICS' not in strata else ''
     
-    return f'https://api.census.gov/data/timeseries/bds?get={var_string}&for={region_string}&YEAR=*'
+    return f'https://api.census.gov/data/timeseries/bds?get={var_string}&for={region_string}&YEAR=*{naics_string}'
 
 
-def _bds_data_create(variables, region, industry_lst):
-    url = _build_url(variables, region)
+def _bds_data_create(variables, region, strata):
+    url = _build_url(variables, region, strata)
     return _fetch_data(url). \
         pipe(_make_header). \
         pipe(lambda x: _county_fips(x) if region == 'county' else x). \
-        query(f'NAICS in {industry_lst}').\
         rename(columns={'county': 'fips', 'state': 'fips', 'us': 'fips', 'YEAR': 'time', 'NAICS': 'naics'}).\
         assign(
             fips=lambda x: '00' if region == 'us' else x['fips'],
             region=lambda x: x['fips'].map(c.all_fips_to_name),
             industry=lambda x: x['naics'].map(c.naics_code_to_abb(2))
         ). \
-        astype({**{var: 'int' for var in variables}, **{'time': 'int'}}).\
+        apply(lambda x: pd.to_numeric(x, errors='ignore') if x.name not in ['fips', 'naics'] else x).\
         sort_values(['fips', 'time']).\
         reset_index(drop=True) \
-        [['fips', 'region', 'naics', 'industry', 'time'] + variables]
+        [['fips', 'region', 'time'] + strata + variables]
 
 
-def bds(series_lst, obs_level='all', industry='00'):
+def bds(series_lst, obs_level='all', strata=[]):
     """ Create a pandas data frame with results from a BDS query. Column order: fips, region, time, series_lst.
 
     Keyword arguments:
@@ -71,24 +72,17 @@ def bds(series_lst, obs_level='all', industry='00'):
         CBSA: Geography
         COUNTY: Geography
         DENOM: (DHS) denominator
-        EAGE: Establishment age code
         EMP: Number of employees
-        EMPSZES: Employment size of establishments code
-        EMPSZESI: Initial employment size of establishments code
-        EMPSZFI: Employment size of firms code
-        EMPSZFII: Initial employment size of firms code
         ESTAB: Number of establishments
         ESTABS_ENTRY: Number of establishments born during the last 12 months
         ESTABS_ENTRY_RATE: Rate of establishments born during the last 12 months
         ESTABS_EXIT: Number of establishments exited during the last 12 months
         ESTABS_EXIT_RATE: Rate of establishments exited during the last 12 months
-        FAGE: Firm age code
         FIRM: Number of firms
         FIRMDEATH_EMP: Number of employees associated with firm deaths during the last 12 months
         FIRMDEATH_ESTABS: Number of establishments associated with firm deaths during the last 12 months
         FIRMDEATH_FIRMS: Number of firms that exited during the last 12 months
         GEO_ID: Geographic identifier code
-        GEOCOMP: GEO_ID Component
         INDGROUP: Industry group
         INDLEVEL: Industry level
         JOB_CREATION: Number of jobs created from expanding and opening establishments during the last 12 months
@@ -113,55 +107,56 @@ def bds(series_lst, obs_level='all', industry='00'):
         ucgid: Uniform Census Geography Identifier clause
         YEAR: Year
 
+    strata--list of variables by which to stratify
+        GEOCOMP: GEO_ID Component
+        EAGE: Establishment age code
+        EMPSZES: Employment size of establishments code
+        EMPSZESI: Initial employment size of establishments code
+        EMPSZFI: Employment size of firms code
+        EMPSZFII: Initial employment size of firms code
+        FAGE: Firm age code
+        NAICS: 2017 NAICS Code
+
         FAGE codes
-                1	Total	0	All firm ages
-            10	0 Years	1	Firms less than one year old
-            20	1 Year	1	Firms one year old
-            25	1-5 Years	2	Firms between one and five years old
-            30	2 Years	1	Firms two years old
-            40	3 Years	1	Firms three years old
-            50	4 Years	1	Firms four years old
-            60	5 Years	1	Firms five years old
-            70	6-10 Years	0	Firms between six and ten years old
-            75	11+ Years	2	Firms eleven or more years old
-            80	11-15 Years	1	Firms between eleven and fifteen years old
-            90	16-20 Years	1	Firms between sixteen and twenty years old
-            100	21-25 Years	1	Firms between twenty one and twenty five years old
-            110	26+ Years	1	Firms twenty six or more years old
-            150	Left Censored	0	"Firms of unknown age (born before 1977)”
+            1   Total   0   All firm ages
+            10  0 Years 1   Firms less than one year old
+            20  1 Year  1   Firms one year old
+            25  1-5 Years   2   Firms between one and five years old
+            30  2 Years 1   Firms two years old
+            40  3 Years 1   Firms three years old
+            50  4 Years 1   Firms four years old
+            60  5 Years 1   Firms five years old
+            70  6-10 Years  0   Firms between six and ten years old
+            75  11+ Years   2   Firms eleven or more years old
+            80  11-15 Years 1   Firms between eleven and fifteen years old
+            90  16-20 Years 1   Firms between sixteen and twenty years old
+            100 21-25 Years 1   Firms between twenty one and twenty five years old
+            110 26+ Years   1   Firms twenty six or more years old
+            150 Left Censored   0   "Firms of unknown age (born before 1977)”
 
-    obs_level-- str or lst of the level of observation(s) to pull at.
-            all:
-            us:
-            state:
-            county:
-            list of regions according to fips code
-
-    industry
-        00:
-        todo: other codes?
+    obs_level--str or lst of the level of observation(s) to pull at.
+        all:
+        us:
+        state:
+        county:
+        list of regions according to fips code
 
     first year available is 1978, last year is 2018
     """
     if type(obs_level) == list:
         region_lst = obs_level
+    elif obs_level in ['us', 'state', 'county']:
+        region_lst = [obs_level]
     else:
-        if obs_level in ['us', 'state', 'county']:
-            region_lst = [obs_level]
-        else:
-            region_lst = ['us', 'state', 'county']
+        region_lst = ['us', 'state', 'county']
 
-    if type(industry) == list:
-        industry_lst = industry
-    else:
-        if industry == 'all':
-            industry_lst = list(c.naics_code_to_abb(2).keys())
-        else:
-            industry_lst = [industry]
+    invalid_strata = set(strata) - {'GEOCOMP', 'EAGE', 'EMPSZES', 'EMPSZESI', 'EMPSZFI', 'EMPSZFII', 'FAGE', 'NAICS'}
+    if invalid_strata:
+        raise Exception(f'Variables {invalid_strata} are invalid inputs to strata argument. Refer to the function documentation for valid strata.')
 
     return pd.concat(
             [
-                _bds_data_create(series_lst, region, industry_lst)
+                _bds_data_create(series_lst, region, strata)
                 for region in region_lst
             ],
             axis=0
